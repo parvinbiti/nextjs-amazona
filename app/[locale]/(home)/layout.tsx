01cfa350835e2158dd198f670e352e2c2765132d
parvinbiti <parvinbiti365@gmail.com>
2025-12-11 02:37:02 -0300

    r

--- app/[locale]/(home)/layout.tsx
+++ app/[locale]/(home)/layout.tsx
@@ -1,16 +1,19 @@
-import Header from '@/components/shared/header'
-import Footer from '@/components/shared/footer'
+import Header from '@/components/shared/header';
+import Footer from '@/components/shared/footer';
+import { Suspense } from 'react';
 
-export default async function HomeLayout({
+export default function HomeLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <Header />
-      <main className='flex-1 flex flex-col'>{children}</main>
+    <div className="flex flex-col min-h-screen">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+      </Suspense>
+      <main className="flex-1 flex flex-col">{children}</main>
       <Footer />
     </div>
-  )
+  );
 }
