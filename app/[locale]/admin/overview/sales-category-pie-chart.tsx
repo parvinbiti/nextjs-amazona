3fef46db39bf72621358ec8b69d9854561d09ca5
parvinbiti <parvinbiti365@gmail.com>
2025-12-11 00:50:28 -0300

    akon

--- app/[locale]/admin/overview/sales-category-pie-chart.tsx
+++ app/[locale]/admin/overview/sales-category-pie-chart.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use client'
+'use client';
 
-import useColorStore from '@/hooks/use-color-store'
-import { useTheme } from 'next-themes'
-import React from 'react'
-import { PieChart, Pie, ResponsiveContainer, Cell } from 'recharts'
+import useColorStore from '@/hooks/use-color-store';
+import { useTheme } from 'next-themes';
+import React from 'react';
+import { PieChart, Pie, ResponsiveContainer, Cell } from 'recharts';
 
 export default function SalesCategoryPieChart({ data }: { data: any[] }) {
-  const { theme } = useTheme()
-  const { cssColors } = useColorStore(theme)
+  const { theme } = useTheme();
+  const { cssColors } = useColorStore(theme);
 
-  const RADIAN = Math.PI / 180
+  const RADIAN = Math.PI / 180;
   const renderCustomizedLabel = ({
     cx,
     cy,
@@ -19,9 +19,9 @@ export default function SalesCategoryPieChart({ data }: { data: any[] }) {
     outerRadius,
     index,
   }: any) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5
-    const x = cx + radius * Math.cos(-midAngle * RADIAN)
-    const y = cy + radius * Math.sin(-midAngle * RADIAN)
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
     return (
       <>
@@ -29,23 +29,23 @@ export default function SalesCategoryPieChart({ data }: { data: any[] }) {
           x={x}
           y={y}
           textAnchor={x > cx ? 'start' : 'end'}
-          dominantBaseline='central'
-          className='text-xs'
+          dominantBaseline="central"
+          className="text-xs"
         >
           {`${data[index]._id} ${data[index].totalSales} sales`}
         </text>
       </>
-    )
-  }
+    );
+  };
 
   return (
-    <ResponsiveContainer width='100%' height={400}>
+    <ResponsiveContainer width="100%" height={400}>
       <PieChart width={400} height={400}>
         <Pie
           data={data}
-          dataKey='totalSales'
-          cx='50%'
-          cy='50%'
+          dataKey="totalSales"
+          cx="50%"
+          cy="50%"
           labelLine={false}
           label={renderCustomizedLabel}
         >
@@ -58,5 +58,5 @@ export default function SalesCategoryPieChart({ data }: { data: any[] }) {
         </Pie>
       </PieChart>
     </ResponsiveContainer>
-  )
+  );
 }
