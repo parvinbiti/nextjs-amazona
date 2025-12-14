414abccb7e911d563b52e54a3db3dfa259ea749b
parvinbiti <parvinbiti365@gmail.com>
2025-12-14 11:04:09 -0300

    mjn

--- app/[locale]/(root)/account/orders/[id]/page.tsx
+++ app/[locale]/(root)/account/orders/[id]/page.tsx
@@ -1,3 +1,53 @@
+// import { notFound } from 'next/navigation';
+// import React from 'react';
+
+// import { auth } from '@/auth';
+// import { getOrderById } from '@/lib/actions/order.actions';
+// import OrderDetailsForm from '@/components/shared/order/order-details-form';
+// import Link from 'next/link';
+// import { formatId } from '@/lib/utils';
+
+// export async function generateMetadata(props: {
+//   params: Promise<{ id: string }>;
+// }) {
+//   const params = await props.params;
+
+//   return {
+//     title: `Order ${formatId(params.id)}`,
+//   };
+// }
+
+// export default async function OrderDetailsPage(props: {
+//   params: Promise<{
+//     id: string;
+//   }>;
+// }) {
+//   const params = await props.params;
+
+//   const { id } = params;
+
+//   const order = await getOrderById(id);
+//   if (!order) notFound();
+
+//   const session = await auth();
+
+//   return (
+//     <>
+//       <div className="flex gap-2">
+//         <Link href="/account">Your Account</Link>
+//         <span>›</span>
+//         <Link href="/account/orders">Your Orders</Link>
+//         <span>›</span>
+//         <span>Order {formatId(order._id)}</span>
+//       </div>
+//       <h1 className="h1-bold py-4">Order {formatId(order._id)}</h1>
+//       <OrderDetailsForm
+//         order={order}
+//         isAdmin={session?.user?.role === 'Admin' || false}
+//       />
+//     </>
+//   );
+// }
 import { notFound } from 'next/navigation';
 import React from 'react';
 
@@ -23,12 +73,12 @@ export default async function OrderDetailsPage(props: {
   }>;
 }) {
   const params = await props.params;
-
   const { id } = params;
 
   const order = await getOrderById(id);
   if (!order) notFound();
 
+  const orderId = order._id.toString();
   const session = await auth();
 
   return (
@@ -38,11 +88,13 @@ export default async function OrderDetailsPage(props: {
         <span>›</span>
         <Link href="/account/orders">Your Orders</Link>
         <span>›</span>
-        <span>Order {formatId(order._id)}</span>
+        <span>Order {formatId(orderId)}</span>
       </div>
-      <h1 className="h1-bold py-4">Order {formatId(order._id)}</h1>
+
+      <h1 className="h1-bold py-4">Order {formatId(orderId)}</h1>
+
       <OrderDetailsForm
-        order={order}
+        order={{ ...order, _id: orderId }}
         isAdmin={session?.user?.role === 'Admin' || false}
       />
     </>
